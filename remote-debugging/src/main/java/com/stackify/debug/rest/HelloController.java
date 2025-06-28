e3d7ac611aa84926cad5036a13860892316152a9
moltogit <matias.miksys@gmail.com>
2025-06-28 00:56:08 -0300

    Fix hello endpoint mapping

--- remote-debugging/src/main/java/com/stackify/debug/rest/HelloController.java
+++ remote-debugging/src/main/java/com/stackify/debug/rest/HelloController.java
@@ -1,10 +1,12 @@
 package com.stackify.debug.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/hello")
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping
